1b60c42906d42fc1aaae9c7dacf6b971eb195960
Michael Haase <darkstar@gernox.de>
2016-02-27 12:29:47 +0100

    Remove ifconfig-pool

--- manifests/server.pp
+++ manifests/server.pp
@@ -22,7 +22,6 @@ class openvpn::server (
   $status_log               = 'openvpn-status.log',
   $plugins                  = '',
   $verb                     = '3',
-  $pool                     = '10.8.0.1 10.8.0.254 255.255.255.0',
   $port                     = '1194',
   $proto                    = 'udp',
   $redirect_gateway         = '',
